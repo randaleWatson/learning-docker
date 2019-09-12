b4ed0026f213d94cc92c002b8aa68012e2df784a
Randale Watson <randalewatson@Randales-Mac-mini.local>
2019-09-11 20:09:41 -0400

    simoke web: making copying more efficient

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hi there</h1>');
+    res.send('<h1>Hi there, my guy</h1>');
 });
 
 app.listen(8080, () => {
