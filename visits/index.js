74febe0f13e6a26c8fa164a45b0ac468180fe968
Randale Watson <randalewatson@Randales-Mac-mini.local>
2019-09-12 00:08:14 -0400

    visits causing container to failed

--- visits/index.js
+++ visits/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
 
@@ -9,13 +10,14 @@ const client = redis.createClient({
 });
 client.set('visits', 0);
 
-app.get('/', (req, res)=>{
-    client.get('visits', (err, visits) =>{
+app.get('/', (req, res) => {
+    process.exit(0);
+    client.get('visits', (err, visits) => {
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1)
     });
 })
 
-app.listen(8081, () =>{
+app.listen(8081, () => {
     console.log('Listening on port: 8081');
 })
